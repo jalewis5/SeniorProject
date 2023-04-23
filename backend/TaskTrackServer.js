2c44d28cf5d9f1a12cf975955eb93c26bdd637e4
jalewis5 <55959369+jalewis5@users.noreply.github.com>
2023-04-23 06:09:02 +0000

    Began creating routes

--- backend/TaskTrackServer.js
+++ backend/TaskTrackServer.js
@@ -6,7 +6,15 @@ const app = express();
 
 
 //ROUTES
+app.get("/", (req, res) => {
+    res.send("Main Page");
+});
 
+//Create a Task
+app.post("/api/tasks", async (req, res) =>{
+    console.log(req.body);
+    res.send("Task created");
+});
 
 //Using port 4000 for now, see if Github will provide our port number
 const PORT = process.env.PORT || 4000;
