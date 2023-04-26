8a4169bf2e0b4b6df708c90db70485aaf8c2d2b5
jalewis5 <55959369+jalewis5@users.noreply.github.com>
2023-04-26 01:52:41 +0000

    Adding routes with middleware

--- backend/TaskTrackServer.js
+++ backend/TaskTrackServer.js
@@ -1,9 +1,13 @@
 const dotenv = require("dotenv").config(); //app now has access to .env file
 const express = require("express");
 const connectDB = require("./config/connectDB");
+const mongoose = require("mongoose");
+const Task = require("./model/TaskModel"); //import task schema
 
 const app = express();
 
+//Middleware used to get access to data
+app.use(express.json())
 
 //ROUTES
 app.get("/", (req, res) => {
@@ -12,8 +16,12 @@ app.get("/", (req, res) => {
 
 //Create a Task
 app.post("/api/tasks", async (req, res) =>{
-    console.log(req.body);
-    res.send("Task created");
+    try {
+        const task = await Task.create(req.body);
+        res.status(200).json(task);
+    } catch (error) {
+        res.status(500).json({msg: error.message});
+    }
 });
 
 //Using port 4000 for now, see if Github will provide our port number
